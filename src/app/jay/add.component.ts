ecc5224963856cb0aefb8a1366c6ed7ad6ec783b
v-jiapji@HW-JIAPJI-01 <soullessl@qq.com>
2017-01-03 17:10:12 +0800

    change add.component

--- src/app/jay/add.component.ts
+++ src/app/jay/add.component.ts
@@ -1,6 +1,6 @@
 import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 import { HeroDataService, HeroClass } from './webapi.component';
 
 @Component({
